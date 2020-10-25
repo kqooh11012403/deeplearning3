8e0cadefcb886a2812405a6719f431e004fef0c3
keisuke <keisuke113124@gmail.com>
2020-10-25 23:56:00 +0900

    commit new file

--- dezero/.ipynb_checkpoints/core-checkpoint.py
+++ dezero/.ipynb_checkpoints/core-checkpoint.py
@@ -109,12 +109,20 @@ class Variable:
             shape = shape[0]
         return dezero.functions.reshape(self, shape)
     
-    def transpose(self):
-        return dezero.funcrions.transpose(self)
+    def transpose(self, *axes):
+        if len(axes) == 0:
+            axes = None
+        elif len(axes) == 1:
+            if isinstance(axes[0], (tuple, list)) or axes[0] is None:
+                axes = axes[0]
+        return dezero.functions.transpose(self, axes)
     
     @property
     def T(self):
-        return dezero.funcrions.transpose(self)
+        return dezero.functions.transpose(self)
+    
+    def sum(self, axis=None, keepdims=False):
+        return dezero.functions.sum(self, axis, keepdims)
     
 class Function:
     def __call__(self, *inputs):
@@ -143,11 +151,16 @@ class Function:
 
 class Add(Function):
     def forward(self, x0, x1):
+        self.x0_shape, self.x1_shape = x0.shape, x1.shape
         y = x0 + x1
         return y
     
     def backward(self, gy):
-        return gy, gy
+        gx0, gx1 = gy, gy
+        if self.x0_shape != self.x1_shape:
+            gx0 = dezero.functions.sum_to(gx0, self.x0_shape)
+            gx1 = dezero.functions.sum_to(gx1, self.x1_shape)
+        return gx0, gx1
 
 def add(x0, x1):
     x1 = as_array(x1)
